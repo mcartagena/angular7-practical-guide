15703f2dbfb669accb204072afc68c0fd7d0ba23
mcartagena <marcelocartagenaorellana@gmail.com>
2019-07-21 07:15:35 -0400

    typescript basico

---
+++ exercises/typescript-basics/01-dividedby12.js
@@ -0,0 +1,13 @@
+// if a number is divided by 12 print de rest of the numbers that are divided by 12
+// if a number is not divide by 12 print the rest of the number substracting -1
+var theNumber = 121;
+if (theNumber % 12 == 0) {
+    for (; theNumber > 0; theNumber = theNumber - 12) {
+        console.log(theNumber);
+    }
+}
+else {
+    for (; theNumber > 0; theNumber = theNumber - 1) {
+        console.log(theNumber);
+    }
+}
---
+++ exercises/typescript-basics/01-dividedby12.ts
@@ -0,0 +1,14 @@
+// if a number is divided by 12 print de rest of the numbers that are divided by 12
+// if a number is not divide by 12 print the rest of the number substracting -1
+
+let theNumber = 121;
+
+if(theNumber%12==0){
+    for(;theNumber>=0;theNumber-=theNumber){
+        console.log(theNumber);
+    }
+} else {
+    for(;theNumber>=0;theNumber--){
+        console.log(theNumber);
+    }
+}
